afafad78c963575386c99f7d6dc200c53632aa2b
Luis Omar Burgos Parra <burgosparraluisomar@gmail.com>
2022-03-23 10:01:04 -0600

    tercer commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 
 
 function App() {
+
+  
+let varprueba = "titulo"
+  const handleChangePrueba = (event) => { 
+varprueba = event.target.value
+
+   
+  
+  }
+
+
   return (
 <div className="App">
 
-<h3>Hola que hace</h3>
+<h3>Notas</h3>
+<label htmlFor="pruebaID">Input de prueba</label>
+
+<input id="pruebaID" name="InPUT" type="text" onChange={handleChangePrueba} value={varprueba}
+/>
 
 </div>
 );
