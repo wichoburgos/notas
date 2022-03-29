ca3bda2c04fdf864164bfd75a834cf59e3aa1c8b
Luis Omar Burgos Parra <burgosparraluisomar@gmail.com>
2022-03-29 09:49:35 -0600

    control de multiples input en una sola entrada

--- src/App.js
+++ src/App.js
@@ -4,21 +4,19 @@ function App() {
 
 //hooks
 
-const [tituloState, setTituloState] =useState("");
-const [fechaState, setfechaState] =useState("");
-const [notaState, setnotaState] =useState("");
+const [inputState, setInputState] =useState({
+  titulo: "",
+  fecha:"",
+  nota:"",
+});
 
-const handleChangetitulo = (event) => {
-  setTituloState(event.target.value);
-}
 
-const handleChangefecha = (event) => {
-  setfechaState(event.target.value);
-}
-
-const handleChangeNota = (event) => {
-  setnotaState(event.target.value);
-}
+const handleInputChange = (event) => {
+  setInputState({
+    ...inputState,
+    [event.target.name]: event.target.value,
+  });
+};
 
 
 
@@ -28,13 +26,13 @@ const handleChangeNota = (event) => {
 <h3>Notas</h3>
 <label htmlFor="titulo">Titulo: </label>
 
-<input id="titulo" name="titulo" type="text" onChange={handleChangetitulo} value={tituloState}
+<input id="titulo" name="titulo" type="text" onChange={handleInputChange} value={inputState.titulo}
 />
 
 <br />
 <label htmlFor="fecha">Fecha: </label>
 
-<input id="fecha" name="fecha" type="text" onChange={handleChangefecha} value={fechaState}
+<input id="fecha" name="fecha" type="text" onChange={handleInputChange} value={inputState.fecha}
 />
 
 <br />
@@ -42,7 +40,7 @@ const handleChangeNota = (event) => {
 
 <label htmlFor="nota">Nota: </label>
 
-<input id="nota" name="nota" type="text" onChange={handleChangeNota} value={notaState}
+<input id="nota" name="nota" type="text" onChange={handleInputChange} value={inputState.nota}
 />
 
 
